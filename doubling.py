4022a9019d65b8060bb7207957c2e696f300e69b
PCain02 <89534001+PCain02@users.noreply.github.com>
2024-04-04 14:43:02 -0400

    fix typo

--- doubling.py
+++ doubling.py
@@ -8,7 +8,7 @@ def doubling(file_name: str, function_name: str, starting_size: int, runs: int) 
     result = getattr(generated_list, file.function_name)()
     end_time = time.time()
     execution_time = end_time - start_time
-    print(f"Run {run_number+1} of {runs} for {operation} operation with singlylinked list using size {startsize} took {execution_time:.10f} seconds")
+    print(f"Run {run_number+1} of {runs} for {operation} operation with list using size {startsize} took {execution_time:.10f} seconds")
     execution_times.append((run_number, startsize, execution_time))
     startsize *= 2 
   return execuion_times
