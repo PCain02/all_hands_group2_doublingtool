d1a1b8594abde69c8664ff93ff53fddd0663b807
Benedek Kaibas <82393336+benedekaibas@users.noreply.github.com>
2024-04-04 14:49:08 -0400

    doubling updated

--- doubling.py
+++ doubling.py
@@ -3,7 +3,7 @@ def doubling(file_name: str, function_name: str, starting_size: int, runs: int) 
   execution_times = []
   # TODO: ADD READING IN OF FILE AND FUNCTION
   for run_number in range(runs):
-    generated_list = generate.generate_random_container(TODO: CONTAINER INPUTS)
+    generated_list = generate.generate_random_container(starting_size, 100)
     start_time = time.time()
     result = getattr(generated_list, file.function_name)()
     end_time = time.time()
