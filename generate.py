8531d09e966c824673e6a8cfa9d2a4c4d6ebe5f5
PCain02 <89534001+PCain02@users.noreply.github.com>
2024-04-04 14:57:08 -0400

    Update generate.py to be able to generate

--- generate.py
+++ generate.py
@@ -4,17 +4,19 @@ import random
 from typing import List, Tuple, Union
 
 
-def generate_random_number(maximum: int, exceed: bool = False) -> int:
+def generate_random_number(maximum: int) -> int:
     """Generate a random number either up to the maximum or exceeding it by one and return it."""
     # generate a random number that is either up to the maximum
     # or exceeding it by one, depending on the input parameter values
-    if exceed is False:
-        num = random.randint(0, maximum)
-    else:
-        num = maximum + 1
+    num = random.randint(0, maximum)
     return num
 
-def generate_random_container(
+def generate_random_str()-> str:
+    list_of_str = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+    num = random.randint(0, 25)
+    return list_of_str[num]
+
+def generate_random_container_int(
     size: int, maximum: int
 ) -> List:
     """Generate a container of a specified size filled with random numbers up to a certain maximum and return either a list or a tuple."""
@@ -22,7 +24,18 @@ def generate_random_container(
     # and with a number up to a specific maximum
     random_list = []
     for i in range(size):
-        # num = generate_random_number(maximum, False)
-        random_list.append(random.randint(0, maximum))
-        # convert the list to a tuple if this was requested
+        num = generate_random_number(maximum)
+        random_list.append(num)
+    return random_list
+
+def generate_random_container_str(
+    size: int
+) -> List:
+    """Generate a container of a specified size filled with random numbers up to a certain maximum and return either a list or a tuple."""
+    #  generate a list of random values for a specific size
+    # and with a number up to a specific maximum
+    random_list = []
+    for i in range(size):
+        letter = generate_random_str()
+        random_list.append(letter)
     return random_list
