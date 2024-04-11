3245b1bcf61b78de2c29cd76a5916b930ebc4302
tuduun <gantulga01@allegheny.edu>
2024-04-11 14:02:43 -0400

    Fix: dd docstring to generate_random_str

--- doubling/generate.py
+++ doubling/generate.py
@@ -12,6 +12,7 @@ def generate_random_number(maximum: int) -> int:
     return num
 
 def generate_random_str()-> str:
+    """Generate a random string based on the alphabet."""
     list_of_str = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
     num = random.randint(0, 25)
     return list_of_str[num]
